12c5ef31e370fa3f4fba78997d6d5cdfa2a1a7c0
sstk14 <mail>
2019-10-22 18:10:58 +0900

    EntityFramework；CodeFirst サンプル

---
+++ CodeFirstDemo/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace CodeFirstDemo
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ CodeFirstDemo/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    public class Post
+    {
+        public int Id { get; set; } 
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
